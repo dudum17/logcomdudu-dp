6404aec50a6f4adbc789a53a4650e7a4fd8bc1bd
dudum17 <eduardom17@al.insper.edu.br>
2026-02-25 15:13:48 -0300

    consetando nomes

--- main.py
+++ main.py
@@ -1,85 +1,84 @@
 import sys
-import re
 
 class Token:
-     def __init__(self, kind, value):
-          self.kind = kind
-          self.value = value
+    def __init__(self, kind, value):
+        self.kind = kind
+        self.value = value
 
 class Lexer:
-     def __init__(self, source, position):
-          self.source = source
-          self.position = position
-          self.next = None
-     def selectNext(self):
-        # Pular espaços
+    def __init__(self, source, position):
+        self.source = source
+        self.position = position
+        self.next = None
+
+    def select_next(self):
         while self.position < len(self.source) and self.source[self.position] == " ":
             self.position += 1
 
-        # Se chegou ao fim da string
         if self.position >= len(self.source):
-            self.next = Token("EOF", '')
+            self.next = Token("EOF", "")
             return
 
         caracter = self.source[self.position]
 
-        # Identificar operadores
-        if caracter == "+":
+        if caracter == '+':
             self.next = Token("PLUS", '+')
             self.position += 1
-        elif caracter == "-":
+
+        elif caracter == '-':
             self.next = Token("MINUS", '-')
             self.position += 1
+
         elif caracter.isdigit():
-            # Ler número
             num = ""
             while self.position < len(self.source) and self.source[self.position].isdigit():
                 num += self.source[self.position]
                 self.position += 1
             self.next = Token("INT", num)
+
         else:
-            # Se encontrou caractere inválido (nem número, nem operador, nem espaço)
-            raise Exception(f"Caractere inválido encontrado: '{caracter}' na posição {self.position}")
+            raise Exception(f"Caractere inválido: '{caracter}' na posição {self.position}")
 
 class Parser:
-     @staticmethod
-     def parseExpression(lex):
-          if lex.next.kind != "INT":
-               raise Exception("ERRO")
-          res = int(lex.next.value)
-          lex.selectNext()
-          while(lex.next.kind == "PLUS" or lex.next.kind == "MINUS"):
-               op = lex.next.kind 
-               lex.selectNext()
-               if lex.next.kind != "INT":
-                    raise Exception("ERRO")
-               if op == "PLUS":
-                    res += int(lex.next.value)
-               elif op == "MINUS":
-                    res -= int(lex.next.value)
-               lex.selectNext()
-          return res
-     @staticmethod
-     def run(code):
-          lex = Lexer(code, 0)
-          lex.selectNext()
-          par = Parser.parseExpression(lex)
-          res = Parser.parseExpression(lex)
-          if (lex.next.kind != "EOF"):
-              raise Exception("expressão invalida")
-          else:
-              return Parser.parseExpression(lex)
-              return res
+    @staticmethod
+    def parse_expression(lex):
+        if lex.next.kind != "INT":
+            raise Exception("ERRO")
 
+        res = int(lex.next.value)
+        lex.select_next()
 
+        while lex.next.kind in ("PLUS", "MINUS"):
+            op = lex.next.kind
+            lex.select_next()
+
+            if lex.next.kind != "INT":
+                raise Exception("ERRO")
+
+            if op == "PLUS":
+                res += int(lex.next.value)
+            else:  # MINUS
+                res -= int(lex.next.value)
+
+            lex.select_next()
+
+        return res
+
+    @staticmethod
+    def run(code):
+        lex = Lexer(code, 0)
+        lex.select_next()
+        res = Parser.parse_expression(lex)
+        if lex.next.kind != "EOF":
+            raise Exception("expressão invalida")
+        return res
 
 def main():
-     if len(sys.argv) != 2:
-        print("Uso: python3 main.py 'expressão'")
+    if len(sys.argv) != 2:
+        print("Uso: python main.py \"expressão\"")
         sys.exit(1)
 
-     print(Parser.run(sys.argv[1]))
-
+    print(Parser.run(sys.argv[1]))
 
 if __name__ == "__main__":
     main()
