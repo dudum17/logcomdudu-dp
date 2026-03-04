4e999da25335d2c84e051039d5086db0280d42ee
dudum17 <eduardom17@al.insper.edu.br>
2026-03-04 15:54:57 -0300

    roteiro 3

--- main.py
+++ main.py
@@ -24,77 +24,104 @@ class Lexer:
         if caracter == '+':
             self.next = Token("PLUS", '+')
             self.position += 1
-
         elif caracter == '-':
             self.next = Token("MINUS", '-')
             self.position += 1
-
-        elif caracter == '^':
-            self.next = Token("XOR", '^')
+        elif caracter == '*':
+            self.next = Token("MULT", '*')
             self.position += 1
-
-
+        elif caracter == '/':
+            self.next = Token("DIV", '/')
+            self.position += 1
+        elif caracter == '(':
+            self.next = Token("OPEN_PAR", '(')
+            self.position += 1
+        elif caracter == ')':
+            self.next = Token("CLOSE_PAR", ')')
+            self.position += 1
         elif caracter.isdigit():
             num = ""
             while self.position < len(self.source) and self.source[self.position].isdigit():
                 num += self.source[self.position]
                 self.position += 1
             self.next = Token("INT", num)
-
         else:
-            raise Exception(f"Caractere inválido: '{caracter}' na posição {self.position}")
             raise Exception("[Lexer] error code")
 
 class Parser:
     lexer = None  # atributo estático
 
+    @staticmethod
+    def parse_factor():
+        if Parser.lexer.next.kind == "INT":
+            res = Parser.lexer.next.value  # <-- corrigido (era Parser.lex)
+            Parser.lexer.select_next()
+            return int(res)
+
+        elif Parser.lexer.next.kind == "PLUS":
+            Parser.lexer.select_next()
+            return Parser.parse_factor()
+
+        elif Parser.lexer.next.kind == "MINUS":
+            Parser.lexer.select_next()
+            return -Parser.parse_factor()
+
+        elif Parser.lexer.next.kind == "OPEN_PAR":
+            Parser.lexer.select_next()
+            res = Parser.parse_expression()
+            if Parser.lexer.next.kind != "CLOSE_PAR":
+                raise Exception("expressão invalida")
+            Parser.lexer.select_next()  # <-- consome o ')'
+            return res
+
+        else:
+            raise Exception("expressão invalida")
 
     @staticmethod
-    def parse_expression():
-        # expressão deve começar com INT
-        if Parser.lexer.next.kind != "INT":
-            raise Exception("[Parser] error code")
+    def parse_term():
+        res = Parser.parse_factor()
 
-        res = int(Parser.lexer.next.value)
-        Parser.lexer.select_next()
-
-        # (PLUS|MINUS INT)*
-        while Parser.lexer.next.kind in ("PLUS", "MINUS", "XOR"):
+        while Parser.lexer.next.kind in ("MULT", "DIV"):
             op = Parser.lexer.next.kind
             Parser.lexer.select_next()
 
-            # depois de + ou - tem que vir INT
-            if Parser.lexer.next.kind != "INT":
-                raise Exception("[Parser] error code")
+            rhs = Parser.parse_factor()
 
-            val = int(Parser.lexer.next.value)
-            if op == "PLUS":
-                res += val
-            elif op == "MINUS":
-                res -= val
-            elif op == "XOR":
-                res ^= val
+            if op == "MULT":
+                res = res * rhs
+            else:  # DIV
+                if rhs == 0:
+                    raise Exception("divisão por zero")
+                res = res // rhs
 
+        return res
+
+    @staticmethod
+    def parse_expression():
+        res = Parser.parse_term()
+        while Parser.lexer.next.kind in ("PLUS", "MINUS"):
+            op = Parser.lexer.next.kind
             Parser.lexer.select_next()
-
-        # terminou a expressão: tem que ser EOF
-        if Parser.lexer.next.kind != "EOF":
-            raise Exception("[Parser] error code")
-
+            if op == "PLUS":
+                res += Parser.parse_term()
+            elif op == "MINUS":
+                res -= Parser.parse_term()
         return res
 
     @staticmethod
     def run(code):
         Parser.lexer = Lexer(code, 0)
         Parser.lexer.select_next()
-        return Parser.parse_expression()
+        res = Parser.parse_expression()
+        if Parser.lexer.next.kind != "EOF":
+            raise Exception(f"expressão invalida, sobrou token: {Parser.lexer.next.kind}")
+        return res
 
 def main():
     if len(sys.argv) != 2:
         print("Uso: python main.py \"expressão\"")
         sys.exit(1)
 
-    # NÃO captura exception: o tester quer ver a Exception acontecer
     print(Parser.run(sys.argv[1]))
 
 if __name__ == "__main__":
