c69e2afe1634c07ab62b6ca84300e027b9c7699b
dudum17 <eduardom17@al.insper.edu.br>
2026-03-11 13:13:40 -0300

    roteiro 4

--- main.py
+++ main.py
@@ -1,9 +1,55 @@
 import sys
+import re
+from abc import ABC, abstractmethod
+from typing import Any
 
 class Token:
     def __init__(self, kind, value):
         self.kind = kind
         self.value = value
+
+class Node(ABC):
+    def __init__(self, value : str, children : list["Node"]):
+        self.value = value
+        self.children = children
+    
+    @abstractmethod
+    def evaluate(self):
+        pass
+
+class IntVal(Node):
+    def __init__(self, value : str ):
+        super().__init__(value, [])
+
+    def evaluate(self):
+        return self.value
+    
+
+class UnOp(Node):
+    def __init__(self, value: str, children: list["Node"]):
+          super().__init__(value, [children])
+
+    def evaluate(self):
+        val = self.children[0].evaluate()
+        if self.value == "+":
+            return val
+        elif self.value == "-":
+            return -(val)
+        
+
+class BinOp(Node):
+    def __init__(self, value: str, children: list["Node"]):
+        super().__init__(value, children)
+    def evaluate(self):
+        if self.value == "+":
+            value = self.children[0].evaluate() + self.children[1].evaluate() 
+        elif self.value == "-":
+            value = self.children[0].evaluate() - self.children[1].evaluate() 
+        elif self.value == "*":
+            value = self.children[0].evaluate() * self.children[1].evaluate() 
+        elif self.value == "/":
+            value = self.children[0].evaluate() // self.children[1].evaluate()
+        return value 
 
 class Lexer:
     def __init__(self, source, position):
@@ -28,12 +74,8 @@ class Lexer:
             self.next = Token("MINUS", '-')
             self.position += 1
         elif caracter == '*':
-           if self.position + 1 < len(self.source) and self.source[self.position + 1] == '*':
-              self.next = Token("POWER", '**')
-              self.position += 2
-           else:
-             self.next = Token("MULT", '*')
-             self.position += 1
+            self.next = Token("MULT", '*')
+            self.position += 1
         elif caracter == '/':
             self.next = Token("DIV", '/')
             self.position += 1
@@ -53,98 +95,75 @@ class Lexer:
             raise Exception("[Lexer] error code")
 
 class Parser:
-    lexer = None  # atributo estático
-
-
-    @staticmethod
-    def parse_atom():
-      if Parser.lexer.next.kind == "INT":
-        res = int(Parser.lexer.next.value)
-        Parser.lexer.select_next()
-        return res
-
-      elif Parser.lexer.next.kind == "OPEN_PAR":
-        Parser.lexer.select_next()
-        res = Parser.parse_expression()
-
-        if Parser.lexer.next.kind != "CLOSE_PAR":
-            raise Exception("[Parser] error code")
-
-        Parser.lexer.select_next()
-        return res
-
-      else:
-        raise Exception("[Parser] error code")
-      
-    @staticmethod
-    def parse_power():
-    # power := atom (POWER power)?
-       res = Parser.parse_atom()
-
-       if Parser.lexer.next.kind == "POWER":
-        Parser.lexer.select_next()
-        expo = Parser.parse_power()  # associativo à direita
-        res = res ** expo
-
-       return res
+    lexer = None
 
     @staticmethod
     def parse_factor():
-        # factor := (PLUS factor) | (MINUS factor) | power
+        if Parser.lexer.next.kind == "INT":
+            res = IntVal(int(Parser.lexer.next.value))
+            Parser.lexer.select_next()
+            return res
 
-       if Parser.lexer.next.kind == "PLUS":
-          Parser.lexer.select_next()
-          return Parser.parse_factor()
-
-       elif Parser.lexer.next.kind == "MINUS":
-          Parser.lexer.select_next()
-          return -Parser.parse_factor()
-
-       return Parser.parse_power()
+        elif Parser.lexer.next.kind == "PLUS":
+            Parser.lexer.select_next()
+            res = UnOp("+", Parser.parse_factor())
+            return res
+        elif Parser.lexer.next.kind == "MINUS":
+             Parser.lexer.select_next()
+             res = UnOp("-", Parser.parse_factor())
+             return res
+        elif Parser.lexer.next.kind == "OPEN_PAR":
+            Parser.lexer.select_next()
+            res = Parser.parse_expression()
+            if Parser.lexer.next.kind != "CLOSE_PAR":
+                raise Exception("[Parser] error code")
+            Parser.lexer.select_next()
+            return res
+        else:
+            raise Exception("[Parser] error code")
 
     @staticmethod
     def parse_term():
-        res = Parser.parse_factor()
+            res = Parser.parse_factor()
 
-        while Parser.lexer.next.kind in ("MULT", "DIV"):
-            op = Parser.lexer.next.kind
-            Parser.lexer.select_next()
+            while Parser.lexer.next.kind in ("MULT", "DIV"):
+               op = Parser.lexer.next.kind
+               Parser.lexer.select_next()
 
-            rhs = Parser.parse_factor()
+               if op == "MULT":
+                   res = BinOp("*", [res, Parser.parse_factor()])
+               elif op == "DIV":
+                  res = BinOp("/", [res, Parser.parse_factor()])
 
-            if op == "MULT":
-                res = res * rhs
-            else:  # DIV
-                if rhs == 0:
-                    raise Exception("[Parser] error code")
-                res = res // rhs
-
-        return res
+            return res
 
     @staticmethod
     def parse_expression():
-        res = Parser.parse_term()
-        while Parser.lexer.next.kind in ("PLUS", "MINUS"):
-            op = Parser.lexer.next.kind
-            Parser.lexer.select_next()
-            if op == "PLUS":
-                res += Parser.parse_term()
-            elif op == "MINUS":
-                res -= Parser.parse_term()
-        return res
+             res = Parser.parse_term()
 
+             while Parser.lexer.next.kind in ("PLUS", "MINUS"):
+               op = Parser.lexer.next.kind
+               Parser.lexer.select_next()
+
+               if op == "PLUS":
+                   res = BinOp("+", [res, Parser.parse_term()])
+               elif op == "MINUS":
+                    res = BinOp("-", [res, Parser.parse_term()])
+
+             return res
     @staticmethod
     def run(code):
         Parser.lexer = Lexer(code, 0)
         Parser.lexer.select_next()
         res = Parser.parse_expression()
+
         if Parser.lexer.next.kind != "EOF":
             raise Exception("[Parser] error code")
-        return res
+        return res.evaluate()
 
 def main():
     if len(sys.argv) != 2:
-        print("Uso: python main.py \"expressão\"")
+        print('Uso: python main.py "expressão"')
         sys.exit(1)
 
     print(Parser.run(sys.argv[1]))
