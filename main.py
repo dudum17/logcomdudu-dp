d66e234edace67c99ccd6280bb0fc4d61f24f6a7
dudum17 <eduardom17@al.insper.edu.br>
2026-04-07 12:52:08 -0300

    roteiro 6

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ class PrePro:
     @staticmethod
     def filter(code):
         return re.sub(r"--[^\n]*", "", code)
-    
+
 class SymbolTable:
     def __init__(self):
         self.table = {}
-    
+
     def set_value(self, var, value):
         self.table[var] = value 
 
@@ -35,7 +35,7 @@ class Node(ABC):
     def __init__(self, value : str, children : list["Node"]):
         self.value = value
         self.children = children
-    
+
     @abstractmethod
     def evaluate(self, st : SymbolTable):
         pass
@@ -46,7 +46,7 @@ class IntVal(Node):
 
     def evaluate(self, st):
         return self.value
-    
+
 
 class UnOp(Node):
     def __init__(self, value: str, children: list["Node"]):
@@ -58,8 +58,9 @@ class UnOp(Node):
             return val
         elif self.value == "-":
             return -(val)
+        elif self.value == "not":
+            return not(val)
 
-        
 
 class BinOp(Node):
     def __init__(self, value: str, children: list["Node"]):
@@ -78,63 +79,85 @@ class BinOp(Node):
             if right == 0:
                 raise Exception("[Semantic] error code")
             return left // right
+        elif self.value == "and":
+             return left and right
+        elif self.value == "or":
+            return left or right
+        elif self.value == "==":
+             return left == right
+        elif self.value == "<":
+             return left < right
+        elif self.value == ">":
+             return left > right
 
 class Identifier(Node):
      def __init__(self, value, children):
          super().__init__(value, children)
      def evaluate(self, st):
          return st.get_value(self.value)
-     
+
 class Print(Node):
     def __init__(self, value, children):
         super().__init__(value, [children])
     def evaluate(self, st):
         val = self.children[0].evaluate(st)
         print(val)
-        return None
-    
+        return val
+
 class Assignment(Node):
     def __init__(self, value: str, children: list["Node"]):
         super().__init__(value, children)
-
     def evaluate(self, st):
         varname = self.children[0].value
         varvalue = self.children[1].evaluate(st)
         st.set_value(varname, varvalue)
+        return varvalue
+    
+
+class IF (Node):
+    def __init__(self, value: str, children: list["Node"]):
+        super().__init__(value, children)
+    def evaluate(self, st):
+        condicao = self.children[0].evaluate(st)
+        if condicao:
+            return self.children[1].evaluate(st)
+        elif len(self.children) > 2:
+            return self.children[2].evaluate(st)
         return None
     
+class WHILE (Node):
+    def __init__(self, value: str, children: list["Node"]):
+         super().__init__(value, children)
+    
+    def evaluate(self, st):
+        res = None
+        while self.children[0].evaluate(st):
+            res = self.children[1].evaluate(st)
+        return res
+    
+class Read(Node):
+     def __init__(self):
+        super().__init__("read", None)
+
+     def evaluate(st):
+         try:
+             valor = int(input())
+             return valor
+         except ValueError:
+             raise Exception("Entrada inválida: esperado um número inteiro")    
 
 class Block(Node):
     def __init__(self, value: str, children: list["Node"]):
         super().__init__(value, children)
     def evaluate(self, st):
-       result = None
-       for child in self.children:
-           result = child.evaluate(st)
-       return result
-
-class Factorial(Node):
-    def __init__(self, value, children):
-        super().__init__(value, children)
-
-    def evaluate(self, st):
-        val = self.children[0].evaluate(st)
-
-        if val < 0:
-            raise Exception("[Semantic] error code")
-
-        result = 1
-        for i in range(1, val + 1):
-            result *= i
-
-        return result
-
+        for child in self.children:
+            child.evaluate(st)
 
 
 class NoOp(Node):
     def __init__(self, value, children):
         super().__init__(value, children)
-    
+
     def evaluate(self, st):
         return None
 
@@ -174,12 +197,19 @@ class Lexer:
        elif caracter == ')':
             self.next = Token("CLOSE_PAR", ')')
             self.position += 1
+       elif caracter == '>':
+            self.next = Token("GT", '>')
+            self.position += 1
+       elif caracter == '<':
+            self.next = Token("LT", '<')
+            self.position += 1
        elif caracter == "=":
-            self.next = Token("ASSIGN", "=")
-            self.position += 1
-       elif caracter == "!":
-            self.next = Token("FACT", "!")
-            self.position += 1
+            if self.position + 1 < len(self.source) and self.source[self.position:self.position+2] == "==":
+                self.next = Token("EQ", "==")
+                self.position += 2
+            else:
+                self.next = Token("ASSIGN", "=")
+                self.position += 1
        elif caracter == "\n":
             self.next = Token("END", " ")
             self.position += 1
@@ -197,6 +227,26 @@ class Lexer:
                 self.position += 1
             if ident == "print":
                 self.next = Token("PRINT", ident)
+            elif ident == "and":
+                self.next = Token("AND", ident)
+            elif ident == "or":
+                self.next = Token("OR", ident)
+            elif ident == "not":
+                self.next = Token("NOT", ident)
+            elif ident == "if":
+                self.next = Token("IF", ident)
+            elif ident == "while":
+                self.next = Token("WHILE", ident)
+            elif ident == "else":
+                self.next = Token("ELSE", ident)
+            elif ident == "read":
+                self.next = Token("READ", ident)
+            elif ident == "then":
+                self.next = Token("OPEN_IF_BRA", ident)
+            elif ident == "do":
+                self.next = Token("OPEN_BRA", ident)
+            elif ident == "end":
+                self.next = Token("CLOSE_BRA", ident)
             else:
                 self.next = Token("IDEN", ident)
        else:
@@ -206,53 +256,71 @@ class Parser:
     lexer = None
     symbol_table = None
 
+    @staticmethod
+    def parse_rexpression():
+        left = Parser.parse_expression()
+        if Parser.lexer.next.kind in ("EQ", "LT", "GT"):
+             op = Parser.lexer.next.kind
+             Parser.lexer.select_next()
+             rigth = Parser.parse_expression()
+             if op == "EQ":
+                 return BinOp("==", [left, rigth])
+             elif op == "LT":
+                  return BinOp("<", [left, rigth])
+             elif op == "GT":
+                  return BinOp(">", [left, rigth])
+        return left
+                 
+
+    
+    @staticmethod
+    def parse_bterm():
+        res = Parser.parse_rexpression()
+        while Parser.lexer.next.kind == "AND":
+            Parser.lexer.select_next()
+            res = BinOp("and", [res, Parser.parse_rexpression()])
+        return res
+
+    @staticmethod
+    def parse_bexpression():
+        res = Parser.parse_bterm()
+        while Parser.lexer.next.kind == "OR":
+            Parser.lexer.select_next()
+            res = BinOp("or", [res, Parser.parse_bterm()])
+        return res
 
 
     @staticmethod
     def parse_factor():
-      if Parser.lexer.next.kind == "PLUS":
-        Parser.lexer.select_next()
-        return UnOp("+", Parser.parse_factorial())
-
-      elif Parser.lexer.next.kind == "MINUS":
-        Parser.lexer.select_next()
-        return UnOp("-", Parser.parse_factorial())
-
-      else:
-        return Parser.parse_factorial()
-
-
-    @staticmethod
-    def parse_factorial():
-        res = Parser.parse_primary()
-        if Parser.lexer.next.kind == "FACT":
-           Parser.lexer.select_next()
-           res = Factorial("!", [res])
-        return res
-        
-
-    @staticmethod
-    def parse_primary():
-      if Parser.lexer.next.kind == "INT":
-        res = IntVal(int(Parser.lexer.next.value), [])
-        Parser.lexer.select_next()
-        return res
-
-      elif Parser.lexer.next.kind == "IDEN":
-        res = Identifier(Parser.lexer.next.value, [])
-        Parser.lexer.select_next()
-        return res
-
-      elif Parser.lexer.next.kind == "OPEN_PAR":
-        Parser.lexer.select_next()
-        res = Parser.parse_expression()
-        if Parser.lexer.next.kind != "CLOSE_PAR":
+        if Parser.lexer.next.kind == "INT":
+            res = IntVal(int(Parser.lexer.next.value), [])
+            Parser.lexer.select_next()
+            return res
+        elif Parser.lexer.next.kind == "IDEN":
+            res = Identifier(Parser.lexer.next.value, [])
+            Parser.lexer.select_next()
+            return res
+        elif Parser.lexer.next.kind == "PLUS":
+            Parser.lexer.select_next()
+            res = UnOp("+", Parser.parse_factor())
+            return res
+        elif Parser.lexer.next.kind == "MINUS":
+             Parser.lexer.select_next()
+             res = UnOp("-", Parser.parse_factor())
+             return res
+        elif Parser.lexer.next.kind == "NOT":
+            Parser.lexer.select_next()
+            res = UnOp("not", Parser.parse_factor())
+            return res
+        elif Parser.lexer.next.kind == "OPEN_PAR":
+            Parser.lexer.select_next()
+            res = Parser.parse_bexpression()
+            if Parser.lexer.next.kind != "CLOSE_PAR":
+                raise Exception("[Parser] error code")
+            Parser.lexer.select_next()
+            return res
+        else:
             raise Exception("[Parser] error code")
-        Parser.lexer.select_next()
-        return res
-
-      else:
-        raise Exception("[Parser] error code")
 
     @staticmethod
     def parse_term():
@@ -283,7 +351,7 @@ class Parser:
                     res = BinOp("-", [res, Parser.parse_term()])
 
              return res
-    
+
 
     @staticmethod
     def parse_statement():
@@ -292,7 +360,7 @@ class Parser:
             Parser.lexer.select_next()
             if Parser.lexer.next.kind == "ASSIGN":
                 Parser.lexer.select_next()
-                expr = Parser.parse_expression()
+                expr = Parser.parse_bexpression()
                 if Parser.lexer.next.kind == "END":
                     Parser.lexer.select_next()
                     return Assignment("=", [ident, expr])
@@ -302,13 +370,13 @@ class Parser:
                     raise Exception("[Parser] error code")
             else:
                 raise Exception("[Parser] error code")
-        
+
         elif Parser.lexer.next.kind == "PRINT":
             Parser.lexer.select_next()
             if Parser.lexer.next.kind != "OPEN_PAR":
                 raise Exception("[Parser] error code")
             Parser.lexer.select_next()
-            expr = Parser.parse_expression()
+            expr = Parser.parse_bexpression()
             if Parser.lexer.next.kind != "CLOSE_PAR":
                 raise Exception("[Parser] error code")
             Parser.lexer.select_next()
@@ -319,11 +387,62 @@ class Parser:
                 return Print("print", expr)
             else:
                 raise Exception("[Parser] error code")
+        elif Parser.lexer.next.kind == "WHILE":
+             Parser.lexer.select_next()
+             cond = Parser.parse_bexpression()
+
+             if Parser.lexer.next.kind != "OPEN_BRA":   # do
+               raise Exception("[Parser] error code")
+             Parser.lexer.select_next()
+
+             children = []
+             while Parser.lexer.next.kind != "CLOSE_BRA":   # end
+               stmt = Parser.parse_statement()
+               if stmt is not None:
+                  children.append(stmt)
+
+             Parser.lexer.select_next()   # consome o end
+             return WHILE("while", [cond, Block("block", children)])
+        elif  Parser.lexer.next.kind == "IF":
+            Parser.lexer.select_next()
+            cond = Parser.parse_bexpression()
+
+            if Parser.lexer.next.kind != "OPEN_IF_BRA":   # then
+              raise Exception("[Parser] error code")
+            Parser.lexer.select_next()
+
+            then_children = []
+            while Parser.lexer.next.kind not in ("ELSE", "CLOSE_BRA"):
+              stmt = Parser.parse_statement()
+              if stmt is not None:
+                 then_children.append(stmt)
+
+            children = [cond, Block("block", then_children)]
+
+            if Parser.lexer.next.kind == "ELSE":
+                Parser.lexer.select_next()
+
+                else_children = []
+                while Parser.lexer.next.kind != "CLOSE_BRA":
+                  stmt = Parser.parse_statement()
+                  if stmt is not None:
+                    else_children.append(stmt)
+
+                children.append(Block("block", else_children))
+
+            if Parser.lexer.next.kind != "CLOSE_BRA":
+                raise Exception("[Parser] error code")
+
+            Parser.lexer.select_next()   # consome o end
+            return IF("if", children)
+        
+        elif Parser.lexer.next.kind == "OPEN_BRA":
+            return Parser.parse_block()
         elif Parser.lexer.next.kind == "END":
             Parser.lexer.select_next()
             return NoOp(None, [])
         else:
-             expr = Parser.parse_expression()
+             expr = Parser.parse_bexpression()
              if Parser.lexer.next.kind == "END":
                   Parser.lexer.select_next()
                   return expr
@@ -331,7 +450,26 @@ class Parser:
                  return expr
              else:
                  raise Exception("[Parser] error code")
+   
 
+    
+
+    @staticmethod
+    def parse_block():
+        if Parser.lexer.next.kind != "OPEN_BRA":
+            raise Exception("[Parser] error code")
+        Parser.lexer.select_next()
+        children = []
+        while Parser.lexer.next.kind not in ("CLOSE_BRA", "EOF"):
+            stmt = Parser.parse_statement()
+            if stmt is not None:
+                children.append(stmt)
+        if Parser.lexer.next.kind != "CLOSE_BRA":
+            raise Exception("[Parser] error code")
+        Parser.lexer.select_next()
+        return Block("block", children)
+    
+   
     @staticmethod
     def parse_program():
          children = []
@@ -339,7 +477,7 @@ class Parser:
              stmt = Parser.parse_statement()
              children.append(stmt)
          return Block("block", children)
-             
+
     @staticmethod
     def run(code):
         Parser.lexer = Lexer(code, 0)
@@ -351,23 +489,20 @@ class Parser:
 
 def main():
     if len(sys.argv) != 2:
+        print('Uso: python main.py "expressão"')
         sys.exit(1)
 
-    codigo = sys.argv[1]
-
+    arquivo = sys.argv[1]
     try:
-        novo_codigo = PrePro.filter(codigo)
-        Parser.symbol_table = SymbolTable()
-        resultado = Parser.run(novo_codigo)
-
-        if resultado is not None:
-            print(resultado)
-
-    except Exception as e:
-        print(str(e))
+        with open(arquivo, "r", encoding="utf-8") as f:
+            codigo = f.read()
+    except FileNotFoundError:
+        print(f"Erro: arquivo '{arquivo}' não encontrado.")
         sys.exit(1)
 
-
+    novo_codigo = PrePro.filter(codigo)
+    Parser.symbol_table = SymbolTable()
+    Parser.run(novo_codigo)
     
 
 
